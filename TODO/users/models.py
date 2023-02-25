d3b703f1b53ed6f2132963ed62333dc9228c54e5
Belchitta <102542483+Belchitta@users.noreply.github.com>
2023-02-25 13:55:01 +0300

    Lesson 3 (#6)
    * usersapp
    * New components users,menu,footer
    * Initial commit
    * New App TodoLists

--- TODO/users/models.py
+++ TODO/users/models.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -9,3 +8,6 @@ class TodoUser(AbstractUser):
     last_name = models.CharField(max_length=64)
     birthday_year = models.PositiveIntegerField(default='0000')
     email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f'{self.user_name} | {self.first_name} | {self.last_name}'
