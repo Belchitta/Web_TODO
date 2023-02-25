3eff2ecc1d0483f54d59e4a61a792fda405b09bc
Belchitta <102542483+Belchitta@users.noreply.github.com>
2023-02-25 20:30:43 +0300

    Lesson 5 (#8)
    Routing

--- TODO/frontend/src/App.js
+++ TODO/frontend/src/App.js
@@ -1,46 +1,88 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import {BrowserRouter as Router} from "react-router-dom";
+import { Routes ,Route } from 'react-router-dom';
+import {ProjectList, ProjectDetail} from './components/Project.js'
+import ToDoList from './components/ToDo.js'
 import UserList from "./components/User.js";
-import axios from 'axios';
 import Footer from './components/Footer.js';
 import Navbar from './components/Menu.js';
+import axios from 'axios';
 
+
+const DOMAIN = 'http://127.0.0.1:8001/api/'
+const get_url = (url) => `${DOMAIN}${url}`
 
 class App extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            'users': []
+            navbarItems: [
+                {name: 'Users', href: '/'},
+                {name: 'Projects', href: '/projects'},
+                {name: 'TODOs', href: '/todos'},
+            ],
+             users: [],
+             projects: [],
+             project: {},
+             todos: []
          }
     }
 
+    getProject(id) {
+    axios.get(get_url(`projects/${id}`))
+          .then(response => {
+                console.log(response.data)
+                this.setState({project: response.data})
+          }).catch(error => console.log(error))
+    }
+
     componentDidMount() {
-        axios.get('http://127.0.0.1:8000/api/users')
+        axios.get(get_url('users/'))
             .then(response => {
-                const users = response.data
-                    this.setState(
-                    {
-                        'users': users
-                    }
-                )
+                this.setState({users: response.data})
+            }).catch(error => console.log(error))
+
+        axios.get(get_url('projects/'))
+            .then(response => {
+                this.setState({projects: response.data.results})
+            }).catch(error => console.log(error))
+
+        axios.get(get_url('todos/'))
+            .then(response => {
+                this.setState({todos: response.data.results})
             }).catch(error => console.log(error))
     }
     render() {
         return (
-            <div>
+            <Router>
                 <header>
                     <Navbar navbarItems={this.state.navbarItems}/>
                 </header>
-                <main role="main" class="flex-shrink-0">
+                <main role="main" className="flex-shrink-0">
                     <div className="container">
-                        <UserList users={this.state.users}/>
+                        <Routes>
+                            <Route exact path='/'>
+                                <UserList users={this.state.users}/>
+                            </Route>
+                            <Route exact path='/projects'>
+                                <ProjectList items={this.state.projects}/>
+                            </Route>
+                            <Route exact path='/todos'>
+                                <ToDoList items={this.state.todos}/>
+                            </Route>
+                            <Route path="/project/:id" children={<ProjectDetail getProject={(id) => this.getProject(id)}
+                                                                                item={this.state.project}/>}/>
+                        </Routes>
                     </div>
                 </main>
                 <Footer/>
-            </div>
-            );
+            </Router>
+
+
+        )
     }
 }
 
