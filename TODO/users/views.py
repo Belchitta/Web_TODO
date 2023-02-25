f5563905c97b2a54770d1aff7c4619ed88ce38d9
Belchitta <misschernenok@gmail.com>
2023-02-25 16:26:53 +0300

    Pagination, filtration

--- TODO/users/views.py
+++ TODO/users/views.py
@@ -1,11 +1,13 @@
-from rest_framework.renderers import JSONRenderer, AdminRenderer
+from rest_framework import mixins, viewsets
 from rest_framework.viewsets import ModelViewSet
 
 from .models import TodoUser
 from .serializers import TodoUserModelSerializer
 
 
-class TodoUserModelViewSet(ModelViewSet):
-    renderer_classes = [AdminRenderer]
-    queryset = TodoUser.objects.all()
+class TodoUserViewSet(mixins.ListModelMixin,
+                      mixins.RetrieveModelMixin,
+                      mixins.UpdateModelMixin,
+                      viewsets.GenericViewSet):
     serializer_class = TodoUserModelSerializer
+    queryset = TodoUser.objects.all()
