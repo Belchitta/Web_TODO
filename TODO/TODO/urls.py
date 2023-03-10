9ba1e22f6311d5187a492578424144a074397bdf
Belchitta <misschernenok@gmail.com>
2023-03-10 15:59:00 +0300

    Lesson_9_Done

--- TODO/TODO/urls.py
+++ TODO/TODO/urls.py
@@ -24,19 +24,11 @@ from rest_framework.routers import DefaultRouter, SimpleRouter
 from TodoLists.views import ToDoViewSet, ProjectViewSet
 from users.views import TodoUserViewSet
 from rest_framework.authtoken import views
-<<<<<<< HEAD
-# from rest_framework_simplejwt.views import (
-#     TokenObtainPairView,
-#     TokenRefreshView,
-#     TokenVerifyView
-# )
-=======
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView
 )
->>>>>>> parent of b0be2de... Lesson_9_Done
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -59,13 +51,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
-<<<<<<< HEAD
-    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-=======
     path('api-auth-token/', views.obtain_auth_token),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
->>>>>>> parent of b0be2de... Lesson_9_Done
-    # path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
